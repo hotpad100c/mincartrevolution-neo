1bcc5f86fedbe60dd72d28990ca40714d553d322
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2026-05-05 19:12:42 +0900

    Working minecarts

--- src/main/java/ml/mypals/minecartrevolution/entity/minecarts/workingcarts/NonInventoryWorkingBlockMinecartEntity.java
+++ src/main/java/ml/mypals/minecartrevolution/entity/minecarts/workingcarts/NonInventoryWorkingBlockMinecartEntity.java
@@ -1,5 +1,8 @@
 package ml.mypals.minecartrevolution.entity.minecarts.workingcarts;
 import ml.mypals.minecartrevolution.entity.minecarts.HasVariantRegularBlockMinecartEntity;
+import ml.mypals.minecartrevolution.registeries.MRMinecarts;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -9,12 +12,16 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.minecart.AbstractMinecart;
 import net.minecraft.world.inventory.*;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.CustomData;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import org.jspecify.annotations.NonNull;
+import org.spongepowered.asm.mixin.Unique;
 
 import javax.annotation.Nullable;
 
@@ -26,12 +33,37 @@ public class NonInventoryWorkingBlockMinecartEntity extends HasVariantRegularBlo
     public NonInventoryWorkingBlockMinecartEntity(EntityType<? extends AbstractMinecart> minecart, Level world, double x, double y, double z, Block blockInside) {
         super(minecart, world, x, y, z, blockInside);
     }
-
+    private Item getItem(){
+        BlockState displayBlock = this.entityData
+                .get(DATA_ID_CUSTOM_DISPLAY_BLOCK)
+                .orElse(Blocks.AIR.defaultBlockState());
+        Block block = displayBlock.getBlock();
+        return switch (block) {
+            case SmithingTableBlock ignored -> MRMinecarts.SMITHING_TABLE_MINECART.item().get();
+            case CraftingTableBlock ignored -> MRMinecarts.CRAFTING_TABLE_MINECART.item().get();
+            case StonecutterBlock ignored -> MRMinecarts.STONECUTTER_MINECART.item().get();
+            case LoomBlock ignored -> MRMinecarts.LOOM_MINECART.item().get();
+            case CartographyTableBlock ignored -> MRMinecarts.CARTOGRAPHY_TABLE_MINECART.item().get();
+            case GrindstoneBlock ignored -> MRMinecarts.GRINDSTONE_MINECART.item().get();
+            case AnvilBlock ignored-> MRMinecarts.ANVIL_MINECART.item().get();
+            case EnchantingTableBlock ignored -> MRMinecarts.ENCHANTING_TABLE_MINECART.item().get();
+            case EnderChestBlock ignored -> MRMinecarts.ENDER_CHEST_MINECART.item().get();
+            default -> super.getPickResult().getItem();
+        };
+    }
+    @Override
+    public @NonNull Item getDropItem() {
+        return getItem();
+    }
+    @Override
+    public @NonNull ItemStack getPickResult(){
+        return getItem().getDefaultInstance();
+    }
 
 
     @Override
     public @NonNull InteractionResult interact(Player player, @NonNull InteractionHand hand, @NonNull Vec3 pos) {
-        if (!player.isSecondaryUseActive()) {
+        if (!player.isSecondaryUseActive() && !player.isSprinting()) {
             BlockState blockState = this.getDisplayBlockState();
             if (this.level().isClientSide()) {
                 return InteractionResult.SUCCESS;
