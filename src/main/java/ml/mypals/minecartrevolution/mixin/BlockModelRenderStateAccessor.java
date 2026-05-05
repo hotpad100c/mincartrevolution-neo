079a5501af2b2e27c27b5320489febe74d92cccf
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-05-05 20:11:01 +0800

    附魔台矿车不再渲染静态书

---
+++ src/main/java/ml/mypals/minecartrevolution/mixin/BlockModelRenderStateAccessor.java
@@ -0,0 +1,12 @@
+package ml.mypals.minecartrevolution.mixin;
+
+import net.minecraft.client.renderer.block.BlockModelRenderState;
+import net.minecraft.client.renderer.special.SpecialModelRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(BlockModelRenderState.class)
+public interface BlockModelRenderStateAccessor {
+    @Accessor("specialRenderer")
+    void setSpecialRenderer(SpecialModelRenderer<?> renderer);
+}
