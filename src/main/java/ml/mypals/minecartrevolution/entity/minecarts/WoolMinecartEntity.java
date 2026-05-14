5e3c16c804ce1724fd38f8f71e64440ec8bf1ed5
Mulatram <mulatram88@outlook.com>
2026-05-14 21:53:35 +0800

    fix: Fixed That The No Gravity Doesn't Work When Minecart's Carrying Wool Except the White Wool, And Fixed That The Wool Minecart Was Displayed As White Wool Minecart

--- src/main/java/ml/mypals/minecartrevolution/entity/minecarts/WoolMinecartEntity.java
+++ src/main/java/ml/mypals/minecartrevolution/entity/minecarts/WoolMinecartEntity.java
@@ -8,18 +8,16 @@ import net.minecraft.world.level.Level;
 public class WoolMinecartEntity extends VariantBlockMinecartEntity {
     public WoolMinecartEntity(EntityType<? extends AbstractMinecart> entityType, Level world) {
         super(entityType, world);
-        this.setNoGravity(true);
     }
     public WoolMinecartEntity(EntityType<? extends AbstractMinecart> entityType, Level world, Item item) {
         super(entityType, world, item);
-        this.setNoGravity(true);
     }
     public WoolMinecartEntity(EntityType<? extends AbstractMinecart> minecart, Level world, double x, double y, double z, Item item) {
         super(minecart, world, x, y, z, item);
-        this.setNoGravity(true);
     }
     @Override
     public void tick() {
         super.tick();
+        this.setNoGravity(true);
     }
 }
