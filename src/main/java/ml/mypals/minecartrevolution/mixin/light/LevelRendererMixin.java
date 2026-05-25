ec67161d66bc92fc4f22546edb101ee3ae5b3a1b
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2026-05-25 20:46:46 +0900

    fix: dynamic light compat

--- src/main/java/ml/mypals/minecartrevolution/mixin/light/LevelRendererMixin.java
+++ src/main/java/ml/mypals/minecartrevolution/mixin/light/LevelRendererMixin.java
@@ -1,35 +1,32 @@
 package ml.mypals.minecartrevolution.mixin.light;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import ml.mypals.minecartrevolution.client.light.DynamicLightsStorage;
 import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.block.BlockAndTintGetter;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockAndLightGetter;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LevelRenderer.class)
 public class LevelRendererMixin {
-    @Inject(
-            method = "getLightCoords(Lnet/minecraft/client/renderer/LevelRenderer$BrightnessGetter;Lnet/minecraft/world/level/BlockAndLightGetter;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;)I",
-            at = @At("RETURN"),
-            cancellable = true
-    )
-    private static void injectHeadCancellableGetLightMapCoordinates(
-            LevelRenderer.BrightnessGetter brightnessGetter, BlockAndLightGetter level, BlockState state, BlockPos pos, CallbackInfoReturnable<Integer> cir
-    ) {
-        if (state.isSolidRender()) return;
-        if (state.emissiveRendering(level, pos)) return;
+    @WrapMethod(
+            method = "getLightCoords(Lnet/minecraft/client/renderer/LevelRenderer$BrightnessGetter;Lnet/minecraft/world/level/BlockAndLightGetter;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;)I"
 
-        final double dynamicLightLevel = DynamicLightsStorage.getLightLevel(pos);
-        final int vanillaLightMap = cir.getReturnValue();
+    )
+    private static int mr$getLightMapCoordinates(
+            LevelRenderer.BrightnessGetter brightnessGetter, BlockAndLightGetter level, BlockState state, BlockPos pos, Operation<Integer> original
+    ) {
+        final int vanillaLightMap = original.call(brightnessGetter, level, state, pos);
 
         final int blockLightCoordinates = vanillaLightMap >> 4 & 0xffff / 16;
-        final int light = (int) (Math.min(0xff, 16 * Math.max(dynamicLightLevel, blockLightCoordinates)));
 
-        cir.setReturnValue(vanillaLightMap & 0xffff_0000 | light);
+        if (state.isSolidRender()) return blockLightCoordinates;
+        if (state.emissiveRendering(level, pos)) return blockLightCoordinates;
+
+        final double dynamicLightLevel = DynamicLightsStorage.getLightLevel(pos);
+        final int light = (int) (Math.min(0xff, 16 * Math.max(dynamicLightLevel, blockLightCoordinates)));
+        return vanillaLightMap & 0xffff_0000 | light;
     }
 }
