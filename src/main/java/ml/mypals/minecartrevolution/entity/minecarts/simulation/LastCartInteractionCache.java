7f9e88ad60bcd37cf84de8f23510c68e0f7ba934
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2026-07-12 12:25:49 +0000

    chore: fix linting issues

--- src/main/java/ml/mypals/minecartrevolution/entity/minecarts/simulation/LastCartInteractionCache.java
+++ src/main/java/ml/mypals/minecartrevolution/entity/minecarts/simulation/LastCartInteractionCache.java
@@ -3,5 +3,5 @@ package ml.mypals.minecartrevolution.entity.minecarts.simulation;
 import ml.mypals.minecartrevolution.entity.minecarts.CompatFriendlyBlockMinecartEntity;
 
 public class LastCartInteractionCache {
-    public static CompatFriendlyBlockMinecartEntity LAST_INTERACTED = null;
+  public static CompatFriendlyBlockMinecartEntity LAST_INTERACTED = null;
 }
--- src/main/java/ml/mypals/minecartrevolution/item/MultiVariantMinecartWithBlockItem.java
+++ src/main/java/ml/mypals/minecartrevolution/item/MultiVariantMinecartWithBlockItem.java
@@ -1,18 +1,9 @@
 package ml.mypals.minecartrevolution.item;
 
-import net.minecraft.client.resources.language.I18n;
-import net.minecraft.core.component.DataComponents;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.ComponentSerialization;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
-import org.jspecify.annotations.NonNull;
 
 public class MultiVariantMinecartWithBlockItem extends MinecartWithBlockItem {
   public MultiVariantMinecartWithBlockItem(Properties settings, Block blockInside) {
     super(settings, blockInside);
   }
-
 }
